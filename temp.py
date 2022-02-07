7409b6c06c3581f757a510ec20bff8508cb97269
First Last <kevin622@yonsei.ac.kr>
2022-02-06 18:46:23 -0800

    video attempt 1

--- temp.py
+++ temp.py
@@ -1,3 +1,27 @@
-import numpy as np
-'ValueError: all input arrays must have the same shape'
-np.stack([[1,2], [3]])
+# import gym
+# from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+# env = gym.make("CartPole-v0")
+# video_recorder = None
+# video_recorder = VideoRecorder(env=env, base_path="env_temp_video", enabled=True)
+
+# env.reset()
+# done = False
+# while not done:
+#     env.render()
+#     video_recorder.capture_frame()
+#     _, _, done, _ = env.step(env.action_space.sample())
+
+# video_recorder.close()
+# video_recorder.enabled = False
+# env.close()
+
+import gym
+from gym.wrappers import Monitor
+env = Monitor(gym.make('CartPole-v0'), './video', force=True)
+state = env.reset()
+done = False
+while not done:
+    action = env.action_space.sample()
+    state_next, reward, done, info = env.step(action)
+env.close()
